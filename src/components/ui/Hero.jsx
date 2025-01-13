370a7eb18244c7827881d1cb7dfa94a2a7bc55d0
OverCode <avalosfigueroapedro@gmail.com>
2025-01-12 19:30:01 -0500

    Actualizar nombre de la tienda a "Shoplyx" en index.html, Hero.jsx y NavBar.jsx

--- src/components/ui/Hero.jsx
+++ src/components/ui/Hero.jsx
@@ -4,7 +4,7 @@ export default function Hero() {
   return (
     <section className="h-screen flex flex-col lg:flex-row items-center justify-center gap-10 lg:gap-20 p-5 lg:p-10">
       <div>
-        <h1 className="text-3xl md:text-4xl lg:text-5xl font-medium text-left mb-1">Bienvenido a <span className="font-bold text-transparent bg-clip-text bg-gradient-to-r from-primary to-accent">Shoply</span></h1>
+        <h1 className="text-3xl md:text-4xl lg:text-5xl font-medium text-left mb-1">Bienvenido a <span className="font-bold text-transparent bg-clip-text bg-gradient-to-r from-primary to-accent">Shoplyx</span></h1>
         <p className='text-sm md:text-base lg:text-lg opacity-80 text-pretty max-w-lg'>Descubre artículos únicos y hechos a mano que aportan estilo y personalidad a tu hogar y armario.</p>
       </div>
       <img src={HeroImage} alt="Shopping" className="w-3/5 max-w-80 md:max-w-96 lg:max-w-none md:w-1/2 lg:w-1/3" />
--- src/components/ui/NavBar.jsx
+++ src/components/ui/NavBar.jsx
@@ -9,7 +9,7 @@ export default function NavBar({ darkMode, toggleDarkMode }) {
     <nav className="fixed w-full flex items-center justify-between px-5 lg:px-10 py-5 bg-primary/20 backdrop-blur border-b-2 border-primary/50 z-10">
       <div className="flex items-center gap-1">
         <img src={darkMode ? shopDark : shopLight} alt="Shop Icon" className="size-11" />
-        <span className="text-3xl font-bold text-primary">Shoply</span>
+        <span className="text-3xl font-bold text-primary">Shoplyx</span>
       </div>
       <button className={`relative size-11 bg-background rounded-full border-2 ${darkMode ? "border-white" : "border-yellow-400"}`} onClick={toggleDarkMode}>
         <img src={moonIcon} alt="Sun Icon" className={`absolute size-7 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 transform transition-all duration-300 ${darkMode ? 'rotate-0 opacity-100 scale-100' : 'rotate-90 opacity-0 scale-50'}`} />
